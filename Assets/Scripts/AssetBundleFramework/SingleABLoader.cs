8143cf55b28befb374616154c5dc0b19f44c069c
JackeyZ <892395128@qq.com>
2019-03-19 22:46:49 +0800

    loader修改

--- Assets/Scripts/AssetBundleFramework/SingleABLoader.cs
+++ Assets/Scripts/AssetBundleFramework/SingleABLoader.cs
@@ -14,7 +14,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
+public delegate void LoadCallback();
 namespace AssetBundleFramework
 {
     public class SingleAssetBundle : System.IDisposable
@@ -22,7 +25,8 @@ namespace AssetBundleFramework
         //引用类：资源加载类
         private AssetLoader _AssetLoader;
         //委托：
-
+        LoadCallback _LoadCallback;
+        
         //AssetBundle 名称
         private string _ABName;
 
@@ -30,11 +34,11 @@ namespace AssetBundleFramework
         private string _ABDownLoadPath;
 
         //构造函数
-        public SingleAssetBundle(string abName)
+        public SingleAssetBundle(string abName, LoadCallback loadCallback)
         {
             _ABName = abName;
             //委托定义
-            
+            _LoadCallback = loadCallback;
             //AB包下载路径
             _ABDownLoadPath = PathTool.GetWWWPath() + "/" + _ABName;
         }
@@ -42,7 +46,17 @@ namespace AssetBundleFramework
         //加载AssetBundle资源包
         public IEnumerator LoadAssetBundle()
         {
-            yield return null;
+            UnityWebRequest request = UnityWebRequest.GetAssetBundle(_ABDownLoadPath);
+            yield return request.Send();
+            //取得ab的方式1
+            //AssetBundle ab_prefab = DownloadHandlerAssetBundle.GetContent(request);
+            //取得ab的方式2
+            AssetBundle ab_prefab = (request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+            _AssetLoader = new AssetLoader(ab_prefab);
+            if(_LoadCallback != null)
+            {
+                _LoadCallback();
+            }
         }
 
         /// <summary>
@@ -53,6 +67,10 @@ namespace AssetBundleFramework
         /// <returns></returns>
         public UnityEngine.Object LoadAsset(string assetName, bool isCache)
         {
+            if (_AssetLoader != null) { 
+                return _AssetLoader.LoadAsset(assetName, isCache);
+            }
+            Debug.LogError(GetType() + "/LoadAsset()/ 参数_AssetLoader==null  ,请检查！");
             return null;
         }
 
